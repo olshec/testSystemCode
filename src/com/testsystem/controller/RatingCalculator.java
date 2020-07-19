a61d6e92a915956801013f4bae51b83a3b4b7339
Oleg Shestakov <olshec@gmail.com>
2020-07-19 14:13:54 +0300

    add RatingCalculator

---
+++ src/com/testsystem/controller/RatingCalculator.java
@@ -0,0 +1,21 @@
+package com.testsystem.controller;
+
+/**
+ * Represents a RatingCalculator.
+ * 
+ * @author Oleg Shestakov
+ * @author olshec@gmail.com
+ * @version 1.0
+ */
+public class RatingCalculator {
+
+	/**
+	 * Creates a Group.
+	 * 
+	 * Default constructor
+	 */
+	public RatingCalculator() {
+		
+	}
+
+}
