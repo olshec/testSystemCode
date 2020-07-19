9d2c87d3f77ae22c376c433fd6fd661a100e81b6
Oleg Shestakov <olshec@gmail.com>
2020-07-19 16:23:23 +0300

    added StudentTest

--- src/com/testsystem/models/StudentTest.java
+++ src/com/testsystem/models/StudentTest.java
@@ -1,5 +1,7 @@
 package com.testsystem.models;
 
+import java.util.List;
+
 /**
  * Represents a table for get answers from student.
  * 
@@ -11,10 +13,64 @@ public class StudentTest {
 
 	private Test test;
 	private Student student;
-	//private 
+	private List<List<Integer>> listAnswers;
 	
 	public StudentTest() {
 		
+	}
+
+	/**
+	 * Gets test
+	 * 
+	 * @return the test
+	 */
+	public Test getTest() {
+		return test;
+	}
+
+	/**
+	 * Sets test
+	 * 
+	 * @param test the test to set
+	 */
+	public void setTest(Test test) {
+		this.test = test;
+	}
+
+	/**
+	 * Gets student
+	 * 
+	 * @return the student
+	 */
+	public Student getStudent() {
+		return student;
+	}
+
+	/**
+	 * Sets student
+	 * 
+	 * @param student the student to set
+	 */
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	/**
+	 * Gets answers
+	 * 
+	 * @return the listAnswers
+	 */
+	public List<List<Integer>> getListAnswers() {
+		return listAnswers;
+	}
+
+	/**
+	 * Sets answers
+	 * 
+	 * @param listAnswers the listAnswers to set
+	 */
+	public void setListAnswers(List<List<Integer>> listAnswers) {
+		this.listAnswers = listAnswers;
 	}
 
 }
