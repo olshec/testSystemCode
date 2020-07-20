724c5ed37821c297e007eed4d61ce12db3012904
Oleg Shestakov <olshec@gmail.com>
2020-07-20 18:04:43 +0300

    add sort by value to RatingCalculator

--- src/com/testsystem/controller/RatingCalculator.java
+++ src/com/testsystem/controller/RatingCalculator.java
@@ -1,7 +1,14 @@
 package com.testsystem.controller;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import com.testsystem.models.Group;
 import com.testsystem.models.Student;
@@ -45,9 +52,9 @@ public class RatingCalculator {
 	 * Gets rating for group. 
 	 * Rating of student = (count*100/(count question*5))/10.
 	 * 
-	 * @param student	the student
-	 * @param testBase 	the database of test
-	 * @return int 		the rating of student
+	 * @param student						the student
+	 * @param testBase 						the database of test
+	 * @return HashMap<User, Integer> 		the rating of student
 	 */
 	public static HashMap<User, Integer> getRatingGroup(Group group, TestBase testBase, UserBase userBase) {
 		List<User> listStudent = new GroupBaseController().getStudentByGroup(userBase, group);
@@ -55,7 +62,32 @@ public class RatingCalculator {
 		for(int i=0;i<listStudent.size();i++) {
 			groupRating.put(listStudent.get(i), 
 					RatingCalculator.getRatingStudent(listStudent.get(i),testBase));
-		    }
+		}
+		groupRating = sortByValue(groupRating);
 		return groupRating;    	
 	}
+	
+	// function to sort hashmap by values 
+    private static HashMap<User, Integer> sortByValue(HashMap<User, Integer> hm) 
+    { 
+        // Create a list from elements of HashMap 
+        List<Map.Entry<User, Integer> > list = 
+               new LinkedList<Map.Entry<User, Integer> >(hm.entrySet()); 
+  
+        // Sort the list 
+        Collections.sort(list, new Comparator<Map.Entry<User, Integer> >() { 
+            public int compare(Map.Entry<User, Integer> o1,  
+                               Map.Entry<User, Integer> o2) 
+            { 
+                return (o1.getValue()).compareTo(o2.getValue()); 
+            } 
+        }); 
+          
+        // put data from sorted list to hashmap  
+        HashMap<User, Integer> temp = new LinkedHashMap<User, Integer>(); 
+        for (Map.Entry<User, Integer> aa : list) { 
+            temp.put(aa.getKey(), aa.getValue()); 
+        } 
+        return temp; 
+    } 
 }
