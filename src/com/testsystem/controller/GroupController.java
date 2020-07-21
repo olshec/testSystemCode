491b0c806c12b734ef3c111a2273e410b18fae5e
Oleg Shestakov <olshec@gmail.com>
2020-07-21 15:59:49 +0300

    fix GroupController

--- src/com/testsystem/controller/GroupController.java
+++ src/com/testsystem/controller/GroupController.java
@@ -1,9 +1,50 @@
 package com.testsystem.controller;
 
+import com.testsystem.models.Group;
+
+/**
+ * Represents a GroupBaseController.
+ * 
+ * @author Oleg Shestakov
+ * @author olshec@gmail.com
+ * @version 1.0
+ */
 public class GroupController {
 
+	private Group group;
+	
+	/**
+	 * Creates a GroupController.
+	 */
 	public GroupController() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	/**
+	 * Creates a GroupController.
+	 * 
+	 * @param group the group
+	 */
+	public GroupController(Group group) {
+		setGroup(group);
+	}
+	
+	/**
+	 * Gets group.
+	 * 
+	 * @return the group
+	 */
+	public Group getGroup() {
+		return group;
+	}
+
+	/**
+	 * Sets group.
+	 * 
+	 * @param group the group to set
+	 */
+	public void setGroup(Group group) {
+		this.group = group;
 	}
 
 }
