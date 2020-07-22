869c0504927763c6b702e319a8a89491db7dd99f
Oleg Shestakov <olshec@gmail.com>
2020-07-22 14:42:45 +0300

    added equals and hashCode to Group

--- src/com/testsystem/models/Group.java
+++ src/com/testsystem/models/Group.java
@@ -43,13 +43,13 @@ public class Group {
 		this.name = name;
 	}
 
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + ((name == null) ? 0 : name.hashCode());
-//		return result;
-//	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
