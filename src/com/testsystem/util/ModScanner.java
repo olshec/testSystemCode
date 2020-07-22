caf04151ae91395f74b6c9ae13bc9da3ae0a6d3e
Oleg Shestakov <olshec@gmail.com>
2020-07-22 10:26:15 +0300

    add comments to ModScanner

--- src/com/testsystem/util/ModScanner.java
+++ src/com/testsystem/util/ModScanner.java
@@ -2,12 +2,25 @@ package com.testsystem.util;
 
 import java.util.Scanner;
 
+/**
+ * This class а singleton for resource java.util.Scanner.
+ * 
+ * @author Oleg Shestakov
+ * @author olshec@gmail.com
+ * @version 1.0
+ */
 public class ModScanner {
 
 	private static Scanner scanner;
 
+	/**
+	 * Private constructor.
+	 */
 	private ModScanner() {}
 
+	/**
+	 * Gets scanner.
+	 */
 	public static Scanner getScanner() {
 		if (scanner == null) {
 			scanner = new Scanner(System.in);
@@ -15,6 +28,9 @@ public class ModScanner {
 		return scanner;
 	}
 	
+	/**
+	 * Closes scanner.
+	 */
 	public static void close() {
 		if (scanner == null) {
 			scanner = new Scanner(System.in);
