44d257e4e9db23e3b1b617f0d06a875caa179c66
Oleg Shestakov <olshec@gmail.com>
2020-11-02 10:37:26 +0300

    added ResultAnswers class

---
+++ src/com/testsystem/model/test/ResultAnswers.java
@@ -0,0 +1,19 @@
+package com.testsystem.model.test;
+
+import com.testsystem.model.Model;
+
+public class ResultAnswers extends Model {
+
+	
+	
+	
+	/**
+	 * Gets name of model.
+	 * 
+	 * @return String return name of model
+	 */
+	@Override
+	public String getNameModel() {
+		return "Question";
+	}
+}
