a09e1072431ced07e7832b920ccad8ff84b1528f
Oleg Shestakov <olshec@gmail.com>
2020-11-02 14:42:17 +0300

    fix ResultAnswers class

--- src/com/testsystem/controller/AnswerController.java
+++ src/com/testsystem/controller/AnswerController.java
@@ -3,9 +3,10 @@ package com.testsystem.controller;
 import java.util.List;
 
 import com.testsystem.model.test.Answer;
+import com.testsystem.model.test.ResultAnswers;
 import com.testsystem.model.test.ResultQuestion;
 
-public class AnswerController{
+public class AnswerController {
 
 	/**
 	 * Checks answers.
@@ -13,49 +14,66 @@ public class AnswerController{
 	 * @param userQuestion
 	 * @param sourceQuestion
 	 */
-	public void checkAnswers(List<Answer> userAnswers, List<Answer> sourceAnswers) {
+	public ResultAnswers checkAnswers(List<Answer> userAnswers, List<Answer> sourceAnswers) {
+		final double maxPercentTrueAnswer = 100;
 		int countAnswer = sourceAnswers.size();
-		double maxPointAnswer = 100;
-		double pointOneAnswer = maxPointAnswer / countAnswer;
-		double points = 0;
-		ResultQuestion resultQuestion = new ResultQuestion();
+		double percentPointOneAnswer = maxPercentTrueAnswer / countAnswer;
+		double percentTrueAnswers = 0;
+		int countTrueAnswer = 0;
+		int countNotTrueAnswer = 0;
+		ResultAnswers resultAnswers = new ResultAnswers(userAnswers);
 		boolean hasInCorrectAnswer = false;
-		for(int i = 0; i< sourceAnswers.size(); i++) {
+		for (int i = 0; i < sourceAnswers.size(); i++) {
 			Answer userAnswer = userAnswers.get(i);
 			Answer sourceAnswer = sourceAnswers.get(i);
 			boolean result = checkAnswer(userAnswer, sourceAnswer);
-			if(result==false) {
+			if (result == false) {
 				hasInCorrectAnswer = true;
-				points-=pointOneAnswer;
+				percentTrueAnswers -= percentPointOneAnswer;
+				countNotTrueAnswer++;
+			} else {
+				countTrueAnswer++;
+				percentTrueAnswers += percentPointOneAnswer;
 			}
-			else {
-				points+=pointOneAnswer;
-			}
 		}
-		if(hasInCorrectAnswer) {
-			points = maxPointAnswer;
-			resultQuestion.setPercentCorrectAnswers(100);
-			resultQuestion.setNumberCorrectAnswers(countAnswer);
+		if (hasInCorrectAnswer) {
+			setOptionsCorrectAnswer(resultAnswers, maxPercentTrueAnswer, countTrueAnswer, 0);
+		} else if (countTrueAnswer <= 0) {
+			setOptionsCorrectAnswer(resultAnswers, 0, 0, countNotTrueAnswer);
+		} else {
+			setOptionsCorrectAnswer(resultAnswers, percentTrueAnswers, countTrueAnswer, countNotTrueAnswer);
 		}
-		else {
-			
-		}
+		return resultAnswers;
 	}
-	
+
 	/**
 	 * Checks answer.
 	 * 
-	 * @param user answer
+	 * @param user   answer
 	 * @param source answer
 	 */
 	private boolean checkAnswer(Answer userAnswer, Answer sourceAnswer) {
-		if(sourceAnswer.isCorrect() == true && userAnswer.getChecked() == true) {
+		if (sourceAnswer.isCorrect() == true && userAnswer.getChecked() == true) {
 			return true;
-		}
-		else if(sourceAnswer.isCorrect() == false && userAnswer.getChecked() == false) {
+		} else if (sourceAnswer.isCorrect() == false && userAnswer.getChecked() == false) {
 			return true;
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Sets options for answers.
+	 * 
+	 * @param resultAnswers        result of answers
+	 * @param maxPercentTrueAnswer double
+	 * @param countTrueAnswer      int
+	 * @param countNotTrueAnswer   int
+	 */
+	private void setOptionsCorrectAnswer(ResultAnswers resultAnswers, double maxPercentTrueAnswer, int countTrueAnswer,
+			int countNotTrueAnswer) {
+		resultAnswers.setPercentCorrectAnswers(maxPercentTrueAnswer);
+		resultAnswers.setNumberCorrectAnswers(countTrueAnswer);
+		resultAnswers.setNumberNotCorrectAnswer(countNotTrueAnswer);
+	}
+
 }
--- src/com/testsystem/model/test/ResultAnswers.java
+++ src/com/testsystem/model/test/ResultAnswers.java
@@ -1,12 +1,106 @@
 package com.testsystem.model.test;
 
+import java.util.List;
+
 import com.testsystem.model.Model;
 
 public class ResultAnswers extends Model {
+	
+	private int numberCorrectAnswers;
+	private int numberNotCorrectAnswer;
+	private double percentCorrectAnswers;
+	private List<Answer> answers;
+	
+	/**
+	 * 
+	 */
+	public ResultAnswers() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * @param numberCorrectAnswers
+	 * @param numberNotCorrectAnswer
+	 * @param percentCorrectAnswers
+	 * @param answers
+	 */
+	public ResultAnswers(List<Answer> answers) {
+		super();
+		this.answers = answers;
+	}
+	
+	/**
+	 * @param numberCorrectAnswers
+	 * @param numberNotCorrectAnswer
+	 * @param percentCorrectAnswers
+	 * @param answers
+	 */
+	public ResultAnswers(int numberCorrectAnswers, int numberNotCorrectAnswer, double percentCorrectAnswers,
+			List<Answer> answers) {
+		super();
+		this.numberCorrectAnswers = numberCorrectAnswers;
+		this.numberNotCorrectAnswer = numberNotCorrectAnswer;
+		this.percentCorrectAnswers = percentCorrectAnswers;
+		this.answers = answers;
+	}
 
-	
-	
-	
+	/**
+	 * @return the numberCorrectAnswers
+	 */
+	public int getNumberCorrectAnswers() {
+		return numberCorrectAnswers;
+	}
+
+	/**
+	 * @param numberCorrectAnswers the numberCorrectAnswers to set
+	 */
+	public void setNumberCorrectAnswers(int numberCorrectAnswers) {
+		this.numberCorrectAnswers = numberCorrectAnswers;
+	}
+
+	/**
+	 * @return the percentCorrectAnswers
+	 */
+	public double getPercentCorrectAnswers() {
+		return percentCorrectAnswers;
+	}
+
+	/**
+	 * @param percentCorrectAnswers the percentCorrectAnswers to set
+	 */
+	public void setPercentCorrectAnswers(double percentCorrectAnswers) {
+		this.percentCorrectAnswers = percentCorrectAnswers;
+	}
+
+	/**
+	 * @return the answers
+	 */
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+
+	/**
+	 * @param answers the answers to set
+	 */
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+
+	/**
+	 * @return the numberNotTrueAnswer
+	 */
+	public int getNumberNotCorrectAnswer() {
+		return numberNotCorrectAnswer;
+	}
+
+	/**
+	 * @param numberNotTrueAnswer the numberNotTrueAnswer to set
+	 */
+	public void setNumberNotCorrectAnswer(int numberNotTrueAnswer) {
+		this.numberNotCorrectAnswer = numberNotTrueAnswer;
+	}
+
 	/**
 	 * Gets name of model.
 	 * 
@@ -16,4 +110,5 @@ public class ResultAnswers extends Model {
 	public String getNameModel() {
 		return "Question";
 	}
+
 }
