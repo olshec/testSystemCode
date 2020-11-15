b50e8aa83d18d3efbbfee524ee18704cff43124e
Oleg Shestakov <olshec@gmail.com>
2020-11-15 16:10:35 +0300

    fixed QuestionController class

--- src/com/testsystem/controller/test/QuestionController.java
+++ src/com/testsystem/controller/test/QuestionController.java
@@ -73,17 +73,5 @@ public class QuestionController {
 	public void setAnswers(List<Answer> answers) {
 		question.setAnswers(answers);
 	}
-	
-	/**
-	 * Returns count of correct answers in question.
-	 * 
-	 * @param question
-	 * @return
-	 */
-	public int getCountCorrectAnswers(Question question) {
-		int countCountCorrectAnswers = 
-				new AnswerController().getCountCorrectAnswers(question.getAnswers());
-		return countCountCorrectAnswers;
-	}
 
 }
